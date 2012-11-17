b0e56609c619c18392c7aa68a3a8c470b8fcf971
Bob Nisco <BobNisco@gmail.com>
2012-11-16 23:12:28 -0500

    Templates, urls, fun stuff

--- resnet_app/models.py
+++ resnet_app/models.py
@@ -7,31 +7,31 @@ class Users(models.Model):
 	grad_year = models.IntegerField()
 	employee = models.BooleanField()
 	email = models.EmailField()
-	phone = models.CharField(max_length=20, null=false)
+	phone = models.CharField(max_length=20, null=False)
 
 class Devices(models.Model):
-	owner = models.ForeignKey('Users', db_index=true)
+	owner = models.ForeignKey('Users', db_index=True)
 	type = models.CharField(max_length=20)
 	password = models.CharField(max_length=100)
 	username = models.CharField(max_length=100)
 	serial = models.CharField(max_length=100)
 
-class Reports(models.Models):
-	owner = models.ForeignKey('Users', db_index=true)
+class Reports(models.Model):
+	owner = models.ForeignKey('Users', db_index=True)
 	problem = models.CharField(max_length=100)
 	description = models.TextField()
 	progress = models.CharField(max_length=15)
 	completed = models.BooleanField()
 
-class Report_Tech(models.Models):
-	report = models.ForeignKey('Reports', db_index=true)
-	tech = models.ForeignKey('Users', db_index=true)
+class Report_Tech(models.Model):
+	report = models.ForeignKey('Reports', db_index=True)
+	tech = models.ForeignKey('Users', db_index=True)
 
-class Status(models.Models):
-	tech = models.ForeignKey('Users', db_index=true)
+class Statuses(models.Model):
+	tech = models.ForeignKey('Users', db_index=True)
 	message = models.TextField()
 
-class Notification(models.Models):
-	tech = models.ForeignKey('Users', db_index=true)
+class Notifications(models.Model):
+	tech = models.ForeignKey('Users', db_index=True)
 	type = models.CharField(max_length=20)
 	notes = models.TextField()
--- resnet_app/views.py
+++ resnet_app/views.py
@@ -1,1 +1,9 @@
 # Create your views here.
+from models import Users, Devices, Reports, Report_Tech, Statuses, Notifications
+
+from django.conf import settings
+from django.template import RequestContext
+from django.shortcuts import render_to_response, get_object_or_404
+
+def index(request):
+        return render_to_response('index.html')
